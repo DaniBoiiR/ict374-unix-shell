9e6a50421a891addc512bd1eedf5da335a543ddb
Daniell R. <daniellereanobusiness@gmail.com>
2026-07-17 18:43:33 +0400

    Commented out input logic

--- part1-unixshell/main.c
+++ part1-unixshell/main.c
@@ -68,7 +68,9 @@ int main(int argc, char *argv[]){
     dup2(saved_stderr, 2);
 
     if (reenactingHistory == 0) {
-      // Breaks out the loop if readLine fails. 
+      // Breaks out the loop if readLine fails. TODO: Uncomment once bug is fixed
+      // if(readLine(inputLine, COMMAND_LINE_SIZE, prompt) == -1) {
+
       if(readLine(inputLine, COMMAND_LINE_SIZE, prompt) == -1) {
         fclose(historyfile);
         break;
@@ -157,9 +159,12 @@ int main(int argc, char *argv[]){
   return 0;
 }
 
+/** 
 // General Input line function to read each user input without pressing enter 
 // Required for arrow key functionality 
 int readLine(char *line, int size, char *prompt){ 
+  int bytes; // Num of bytes read 
+  line[0] = '\0';
   char ch; // User entered character 
   int length = 0; // Number of characters in line 
   int cursor = 0; // Cursor position 
@@ -171,21 +176,28 @@ int readLine(char *line, int size, char *prompt){
   raw.c_lflag &= ~(ECHO | ECHOE | ICANON); // Disable Canonical mode and Echoing 
   tcsetattr(0, TCSANOW, &raw); // Set raw mode immediately 
   
-  // Input will keep looping until newline is entered or error in read occurs  
-  while(read(0, &ch, 1) == 1){
-     printf("%s", prompt);
-
+  printf("%s", prompt); // Print prompt first before characters 
+  fflush(stdout); // Flush output buffer 
+  
+  // Input will keep looping until newline is entered or error in read occurs 
+  // Prints each character to the terminal 
+  while((bytes = read(0, &ch, 1)) == 1){
     // For Normal Characters 
     if(ch >= 32 && ch <= 126){
-      if(length < size - 1){
+      if(length < size - 2){ // TODO: Maybe change to -1. -2 is for newline and null 
+        // Move memory first 
+        memmove(&line[cursor+1], &line[cursor], length - cursor + 1);
         line[cursor] = ch; // Enter the character in the current cursor position  
         cursor++;
         length++; 
         
         line[length] = '\0'; // Length will not change position. Always keep as null terminator 
-        printf("%c", ch); 
+        printf("%s", &line[cursor-1]); // Print string up until currently inserted character 
+        for(int n = cursor; n < length; n++){
+          printf("\033[D"); // Print cursor 
+        }
+        
         fflush(stdout);
-        continue; 
       }
     }
     
@@ -193,7 +205,7 @@ int readLine(char *line, int size, char *prompt){
     if(ch == 127){
       // Reduce length and memory if cursor is not at the start of string 
       if(cursor > 0){
-        cursor--; 
+        cursor--;  
         length--; 
 
         // memmove is used to copy block of memory from one place to another 
@@ -203,8 +215,10 @@ int readLine(char *line, int size, char *prompt){
         // length - cursor + 1 is the length of the memory block 
         // We move the character after the current character to the position of the current character. 
         memmove(&line[cursor], &line[cursor+1], length - cursor + 1);
-        printf("\b \b");
-        fflush(stdout);
+        printf("\033[D"); // Move cursor left 
+        printf("%s", &line[cursor]); // Redraw remaining line 
+        printf("\033[K"); // Clear line 
+        fflush(stdout); 
       }
     }
     
@@ -224,25 +238,44 @@ int readLine(char *line, int size, char *prompt){
       if(read(STDIN_FILENO, &seq[0], 1) == 1 && read(STDIN_FILENO, &seq[1], 1) == 1){
         
         // Switch Case statement for arrow keys 
-        if(seq[0] != '[') break; 
-        switch(seq[1]){
-          // Replay History 
-          case 'A': 
-            // TODO: Add a function to get the history by 1 line before but dont execute 
-            break; 
-          case 'B':
-            // TODO: Add a function to get the the next history. Exits with one line.
-            break; 
-          default:
-            break; 
+        if(seq[0] == '['){ 
+          switch(seq[1]){
+            // Replay History 
+            case 'A': 
+              // TODO: Add a function to get the history by 1 line before but dont execute 
+              break; 
+            case 'B':
+              // TODO: Add a function to get the the next history. Exits with one line.
+              break; 
+            case 'C':
+              if(cursor < length){
+                cursor++; 
+                printf("\033[C");
+              }
+              break; 
+            case 'D':
+              if(cursor > 0){
+                cursor--; 
+                printf("\033[D");
+              }
+              break; 
+            default:
+              break; 
+          }
         }
       }
     } 
   }
 
+  if(bytes == 0 ){
+    tcsetattr(0, TCSANOW, &oldToi); // Revert back to canonical mode 
+    return -1;
+  }
+
   tcsetattr(0, TCSANOW, &oldToi); // Revert back to canonical mode 
   return length; 
 }
+*/ 
 
 // Save History
 // TODO: ADD to the built in commands instead of putting this on main 
